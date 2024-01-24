a5672fb7970700c2cbd199613f01047ae37d629f
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-01-24 17:18:10 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -6,15 +6,21 @@ using namespace std;
 int l;
 string s;
 long long ans;
+long long dp[51];
 
 int main() {
 	cin >> l >> s;
 
-	for (int i = 0; i < l; i++) {
-		ans += ((s[i] - 96) * ((long long)powl(31, i) % 1234567891)) % 1234567891;
+	dp[0] = 1;
+	for (int i = 1; i< l; i++) {
+		dp[i] = dp[i - 1] * 31 % 1234567891;
 	}
 
-	cout << ans;
+	for (int i = 0; i < l; i++) {
+		ans += (s[i] - 96) * dp[i] % 1234567891;
+	}
+
+	cout << ans % 1234567891;
 
 	return 0;
 }
