3dd8d8396f05c3876824948819e8125c3e15331c
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-05-28 15:08:56 +0900

    [Gold V] Title: Smooth numbers, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/29543. Smooth numbers/Smooth numbers.cc
+++ 백준/Gold/29543. Smooth numbers/Smooth numbers.cc
@@ -1,22 +1,15 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int l;
-string num;
+int arr[8] = {9, 99, 989, 9889, 97679, 976679, 9643469, 96433469};
 
 int main() {
     cin >> l;
 
-    if (l > 8) {
-        cout << "-1";
-        return 0;
-    }
-
-    int arr[8] = {9, 99, 989, 9889, 97679, 976679, 9643469, 96433469};
-    
-    cout << arr[l - 1];
+    if (l > 8) cout << "-1";
+    else cout << arr[l - 1];
 
     return 0;
 }
