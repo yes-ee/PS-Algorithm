149d510e616e7f9eb79c0edb769cbed42e102113
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-09-16 21:18:12 +0900

    [Silver I] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.cc
+++ 백준/Silver/2527. 직사각형/직사각형.cc
@@ -2,67 +2,28 @@
 
 using namespace std;
 
-char ans;
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+	
 	int x1, y1, p1, q1, x2, y2, p2, q2;
+	
 	for (int i = 0; i < 4; i++) {
 		cin >> x1 >> y1 >> p1 >> q1 >> x2 >> y2 >> p2 >> q2;
 
-		if (x1 > x2) {
-			swap(x1, x2);
-			swap(y1, y2);
-			swap(p1, p2);
-			swap(q1, q2);
-		}
-		if (x1 == x2) {
-			if (y1 > y2) {
-				swap(x1, x2);
-				swap(y1, y2);
-				swap(p1, p2);
-				swap(q1, q2);
-			}
-			if (y1 == y2) {
-				ans = 'a';
-			} else {
-				if (q1 < y2) {
-					ans = 'd';
-				} else if (q1 == y2) {
-					ans = 'b';
-				} else {
-					ans = 'a';
-				}
-			}
-		} else {
-			if (p1 < x2) {
-				ans = 'd';
-			} else if (p1 == x2) {
-				if (q1 < y2) {
-					ans = 'd';
-				} else if (q1 == y2) {
-					ans = 'c';
-				} else {
-					if (q2 < y1) {
-						ans = 'd';
-					} else if (y1 == q2) {
-						ans = 'c';
-					} else {
-						ans = 'b';
-					}
-				}
-			} else {
-				if (y2 == q1 || q2 == y1) {
-					ans = 'b';
-				} else if (y2 > q1 || q2 < y1) {
-					ans = 'd';
-				} else {
-					ans = 'a';
-				}
-			}
-		}
-		cout << ans << "\n";
+		int xl = max(x1, x2);
+		int xr = min(p1, p2);
+		int yd = max(y1, y2);
+		int yu = min(q1, q2);
+
+		int xdiff = xr - xl;
+		int ydiff = yu - yd;
+
+		if (xdiff > 0 && ydiff > 0) cout << "a\n"; // 직사각형
+		else if (xdiff == 0 && ydiff == 0) cout << "c\n"; // 점
+		else if (xdiff < 0 || ydiff < 0) cout << "d\n"; // 겹치지 않음
+		else cout << "b\n"; // 직선
 	}
+
 	return 0;
 }
