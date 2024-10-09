b2807783b940aa3bb46ea97ba19804768af8ac34
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-10-10 00:41:05 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int n, k;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	if (n % 2) cout << "SK";
+	else cout << "CY";
+
+	return 0;
+}
