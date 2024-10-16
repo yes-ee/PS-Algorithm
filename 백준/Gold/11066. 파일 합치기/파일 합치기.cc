9df9a12a441b9408a712513102c15a01c3840b99
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-10-16 19:50:33 +0900

    [Gold III] Title: 파일 합치기, Time: 72 ms, Memory: 3984 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -3,28 +3,16 @@
 using namespace std;
 using ll = long long;
 
-int t;
-int k;
+// bottom up
+
+int t, k;
 ll dp[501][501];
-ll sum[501]; // 파일 누적합
-
-ll re(int st, int end) {
-	if (st == end) return 0;
-	if (dp[st][end]) return dp[st][end];
-
-	ll ms = 1e9;
-
-	for (int i = st; i < end; i++) {
-		ms = min(ms, re(st, i) + re(i + 1, end));
-	}
-
-	return dp[st][end] = ms + sum[end] - sum[st - 1];
-}
+int sum[501];
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	
+
 	cin >> t;
 
 	int a;
@@ -41,9 +29,19 @@ int main() {
 		for (int i = 1; i <= k; i++) {
 			cin >> a;
 			sum[i] = sum[i - 1] + a;
+
 		}
 
-		re(1, k);
+		for (int gap = 1; gap < k; gap++) {
+			for (int st = 1; st + gap <= k; st++) {
+				ll ms = 1e9;
+				for (int i = st; i < st + gap; i++) {
+					ms = min(dp[st][i] + dp[i + 1][st + gap], ms);
+				}
+				dp[st][st + gap] = ms + sum[st + gap] - sum[st - 1];
+			}
+		}
+
 
 		cout << dp[1][k] << "\n";
 	}
