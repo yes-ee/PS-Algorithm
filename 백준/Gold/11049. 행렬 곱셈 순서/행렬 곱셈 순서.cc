b432aaa26ef593704bd63dbaa9a46c66ea93c8da
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-10-23 15:28:45 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 76 ms, Memory: 3984 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+struct m {
+	int x;
+	int y;
+};
+
+int n, r, c;
+ll dp[501][501];
+m mt[501];
+
+void re(int st, int end) {
+	if (st == end) {
+		return;
+	}
+
+	for (int i = st; i < end; i++) {
+		if (dp[st][i] == 1e9) re(st, i);
+		if (dp[i + 1][end] == 1e9) re(i + 1, end);
+		dp[st][end] = min(dp[st][end], dp[st][i] + dp[i + 1][end] + mt[st].x * mt[i].y * mt[end].y);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> mt[i].x >> mt[i].y;
+		for (int j = 1; j <= n; j++)
+			dp[i][j] = 1e9;
+		dp[i][i] = 0;
+	}
+
+
+	re(1, n);
+
+	cout << dp[1][n];
+
+	return 0;
+}
