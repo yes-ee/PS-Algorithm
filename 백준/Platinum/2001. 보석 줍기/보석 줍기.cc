7cb2c4d7300c06c9a32fdd8b861d98c232e2cc79
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-11-14 01:11:04 +0900

    [Platinum V] Title: 보석 줍기, Time: 172 ms, Memory: 7636 KB -BaekjoonHub

--- 백준/Platinum/2001. 보석 줍기/보석 줍기.cc
+++ 백준/Platinum/2001. 보석 줍기/보석 줍기.cc
@@ -15,9 +15,8 @@ vector<pii> v[101]; // 그래프 - end, max cnt
 int getSum(int cur) {
 	int cnt = 0;
 	while (cur) {
-		if (cur & 1) {
+		if (cur & 1)
 			cnt++;
-		}
 		cur = cur >> 1;
 	}
 
@@ -26,16 +25,16 @@ int getSum(int cur) {
 
 void bfs() {
 	queue<pii> q;
-	q.push({ 1, 0 });
+	q.push({1, 0});
 
 	// 1번에 보석 있으면
-	if (jew[1] != -1) {
-		q.push({ 1, 1 << jew[1] });
+	if (jew[1]) {
+		q.push({1, 1 << jew[1]});
 		ans = 1;
 	}
 
 	while (!q.empty()) {
-		int st = q.front().first; // node
+		int st = q.front().first;	// node
 		int cur = q.front().second; // cur state
 		int cnt = getSum(cur);
 		q.pop();
@@ -54,37 +53,35 @@ void bfs() {
 			// 보석 안 주움
 			if (!visited[nx][cur]) {
 				visited[nx][cur] = true;
-				q.push({ nx, cur });
+				q.push({nx, cur});
 			}
 			// 보석 주움
 			int ns = cur | (1 << jew[nx]);
-			if (jew[nx] != -1 && (!visited[nx][ns])) {
+			if (jew[nx] && (!visited[nx][ns])) {
 				visited[nx][ns] = true;
-				q.push({ nx, ns });
+				q.push({nx, ns});
 			}
 		}
 	}
 }
 
-
-int main() {
+int main()
+{
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n >> m >> k;
 
 	int a, b, c;
 
-	for (int i = 0; i <= n; i++) jew[i] = -1;
-
-	for (int i = 0; i < k; i++) {
+	for (int i = 1; i <= k; i++) {
 		cin >> a;
 		jew[a] = i;
 	}
 
 	for (int i = 0; i < m; i++) {
 		cin >> a >> b >> c;
-		v[a].push_back({ b, c});
-		v[b].push_back({ a, c });
+		v[a].push_back({b, c});
+		v[b].push_back({a, c});
 	}
 
 	bfs();
