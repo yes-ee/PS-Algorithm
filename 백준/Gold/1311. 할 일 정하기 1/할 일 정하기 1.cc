f730aba7aae38254c79c8cbbd9947ac84a3ae174
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2024-11-18 16:43:10 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 376 ms, Memory: 174052 KB -BaekjoonHub

---
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int n;
+vector<int> d[21];
+int dp[1 << 21][21];
+
+int dfs(int mask, int x) {
+	if (x == n + 1) return 0;
+
+	if (dp[mask][x]) return dp[mask][x];
+
+	dp[mask][x] = 1e9;
+
+	for (int i = 1; i <= n; i++) {
+		// 이미 한 일이면 패스
+		if (mask & (1 << i)) continue;
+
+		dp[mask][x] = min(dp[mask][x], dfs(mask | (1 << i), x + 1) + d[x][i]);
+	}
+
+	return dp[mask][x];
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int a;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		d[i].push_back(0);
+		for (int j = 0; j < n; j++) {
+			cin >> a;
+			d[i].push_back(a);
+		}
+	}
+
+	cout << dfs(1, 1);
+
+	return 0;
+}
