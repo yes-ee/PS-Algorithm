2f131ff3cf77d67f05b87b64f226695cf9502d6b
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-02-22 09:03:49 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,5 +1,5 @@
--- 코드를 작성해주세요
-SELECT ITEM_ID, ITEM_NAME
-FROM ITEM_INFO
-WHERE ITEM_ID IN (SELECT ITEM_ID FROM ITEM_TREE WHERE PARENT_ITEM_ID IS NULL)
-ORDER BY ITEM_ID
+select a.item_id, item_name
+from item_info a
+join item_tree b
+on a.item_id = b.item_id
+where parent_item_id is null
