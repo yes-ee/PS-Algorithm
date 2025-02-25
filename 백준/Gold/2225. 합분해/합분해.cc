961088704fb13af99f1a78a22ed30e48c8b6ee10
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-02-25 18:51:41 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -1,35 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 using ll = long long;
 
 int n, k;
-int dp[201][201];
+ll dp[201][201];
+ll sum;
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> n >> k;
+    cin >> n >> k;
+    for (int i = 0; i <= n; i++) {
+        dp[1][i] = 1;
+    }
 
-	for (int i = 0; i <= n; i++) {
-		dp[1][i] = 1;
-	}
+    for (int i = 2; i <= k; i++) {
+        sum = 0;
+        for (int j = 0; j <= n; j++) {
+            sum += dp[i-1][j];
+            sum %= 1000000000;
+            dp[i][j] = sum;
+        }
+    }
 
-	for (int i = 0; i <= k; i++) {
-		dp[i][0] = 1;
-	}
+    cout << dp[k][n];
 
-	for (int i = 2; i <= k; i++) {
-		for (int j = 1; j <= n; j++) {
-			for (int k = 0; k <= j; k++) {
-				dp[i][j] += dp[i - 1][j - k];
-				dp[i][j] %= 1000000000;
-			}
-		}
-	}
-
-	cout << dp[k][n];
-
-	return 0;
+    return 0;
 }
