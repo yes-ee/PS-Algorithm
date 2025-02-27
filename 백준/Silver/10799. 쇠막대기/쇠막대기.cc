2114b414d1c399302b59f58b62537e6ededb1aed
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-02-27 20:28:48 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,32 +1,32 @@
 #include <iostream>
+#include <stack>
 #include <string>
 
 using namespace std;
 
-int cnt, sum;
+string s;
+stack<char> st;
+int ans;
 
 int main() {
-	string s;
-	char pre = 'a';
-	cin >> s;
-	for (char c : s) {
-		if (c == ')') {
-			if (pre == '(') {
-				cnt--;
-				sum += cnt;
-			}
-			else {
-				sum++;
-				cnt--;
-			}
-		}
-		else {
-			cnt++;
-		}
-		pre = c;
-	}
+    ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cout << sum;
+    cin >> s;
+    int len = s.length();
+    
+    for (int i = 0; i < len - 1; i++) {
+        if (s[i] =='(' && s[i+1] == ')') { // 레이저
+            ans += st.size();
+            i++;
+        } else if (s[i] == '(') {
+            st.push(s[i]);
+        } else {
+            st.pop();
+            ans++;
+        }
+    }
+    if (s[len-2] != '(') ans++;
+    cout << ans;
 
-	return 0;
+    return 0;
 }
