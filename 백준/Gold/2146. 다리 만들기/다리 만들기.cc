986fa2aff31d36d244dc587621c1c13e9d1dbf82
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-03-04 11:09:16 +0900

    [Gold III] Title: 다리 만들기, Time: 56 ms, Memory: 2144 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.cc
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -11,6 +11,7 @@ using pii = pair<int, int>;
 int n, m;
 int arr[101][101];
 int vis[101][101];
+int chk[101][101];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int ans = 30000;
@@ -26,6 +27,40 @@ int chk_dist(int x, int y, int g) {
         }
     }
     return dis;
+}
+
+int min_bridge(int g) {
+    queue<pii> q;
+    int dis[101][101];
+    for (int i = 0; i< n; i++) {
+        fill(vis[i], vis[i] + n, 0);
+    }
+    // 같은 섬 전부 추가
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == g) {
+                q.push({i, j});
+                vis[i][j] = 1;
+                chk[i][j] = 1;
+            }
+        }
+    }
+
+    while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.X + dx[i];
+            int ny = cur.Y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if (vis[nx][ny]) continue;
+            if (arr[nx][ny] > 0) return dis[cur.X][cur.Y];
+
+            q.push({nx, ny});
+            vis[nx][ny] = 1;
+            dis[nx][ny] = dis[cur.X][cur.Y] + 1;
+        }
+    }
+    return 30000;
 }
 
 void bfs(int x, int y) {
@@ -64,10 +99,12 @@ int main() {
         }
     }
 
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (arr[i][j] == 0) continue;
-            ans = min(chk_dist(i, j, arr[i][j]), ans);
+            if (arr[i][j] == 0 || chk[i][j]) continue;
+            ans = min(min_bridge(arr[i][j]), ans);
+            // ans = min(chk_dist(i, j, arr[i][j]), ans);
         }
     }
 
