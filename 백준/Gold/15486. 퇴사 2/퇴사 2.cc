b1445c588045d74975fbfe199677433ca461cf39
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-03-06 22:38:39 +0900

    [Gold V] Title: 퇴사 2, Time: 192 ms, Memory: 19612 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -10,13 +10,15 @@ int p[1501000];
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> t[i] >> p[i];
-    }
+    // for (int i = 1; i <= n; i++) {
+    //     cin >> t[i] >> p[i];
+    // }
 
     for (int i = 1; i <= n; i++) {
+        int t, p;
+        cin >> t >> p;
         dp[i] = max(dp[i-1], dp[i]);
-        dp[i + t[i] - 1] = max(dp[i + t[i] - 1], dp[i-1] + p[i]);
+        dp[i + t - 1] = max(dp[i + t - 1], dp[i-1] + p);
     }
 
     cout << dp[n];
