d110b6bb08611dcd740b4d79ab83d30a522eac34
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-03-17 01:19:12 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << s << "::fan:\n";
+    cout << ":fan::fan::fan:";
+    return 0;
+}
