00bee8585c942a697b8e7bef054a466ed11ba0f7
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-03-20 20:14:05 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 108 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.cc
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.cc
@@ -1,55 +1,51 @@
 // union find
 
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
 using namespace std;
 
-int t, n, k, a, b, m;
-int p[1000000];
+int p[1000001];
+int t, n, k, a, b;
 
-int find(int u)
-{
-	if (p[u] == u)
-		return u;
-
-	return p[u] = find(p[u]);
+int find(int x) {
+    if (p[x] < 0) return x;
+    return p[x] = find(p[x]);
 }
 
-void merge(int u, int v)
-{
-	u = find(u);
-	v = find(v);
-
-	if (u == v)
-		return;
-
-	p[v] = u;
+bool uni(int u, int v) {
+    u = find(u); v = find(v);
+    if (u == v) {
+        return false;
+    }
+    p[u] = v;
+    return  true;
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cout << "Scenario " << i + 1<< ":\n";
+        cin >> n;
+        fill(p, p + n, -1);
+        cin >> k;
+        for (int j =0; j<k; j++) {
+            cin >> a >> b;
+            uni(a, b);
+        }
+        cin >> k;
+        for (int j =0; j<k; j++) {
+            cin >> a >> b;
+            if (find(a) == find(b)) cout << "1\n";
+            else cout << "0\n";
+        }
+        cout << "\n";
+    }
 
-	for (int i = 0; i < t; i++) {
-		cout << "Scenario " << i + 1 << ":\n";
-		cin >> n >> k;
-
-		for (int i = 0; i <= n; i++)
-			p[i] = i;
-
-		for (int i = 0; i < k; i++) {
-			cin >> a >> b;
-			merge(a, b);
-		}
-
-		cin >> m;
-		for (int i = 0; i < m; i++) {
-			cin >> a >> b;
-			cout << (find(a) == find(b) ? "1" : "0") << "\n";
-		}
-
-		cout << "\n";
-	}
+    return 0;
 }
