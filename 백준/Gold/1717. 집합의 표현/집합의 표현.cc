0d355a394c8e0958f0c2f2bb55459b22e976ef83
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-03-20 19:54:12 +0900

    [Gold V] Title: 집합의 표현, Time: 36 ms, Memory: 6272 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,48 +1,48 @@
-#include <iostream>
+// union find
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
 using namespace std;
 
+int p[1000001];
 int n, m;
-int parent[1000000];
+int cmd, a, b;
 
 int find(int x) {
-	if (parent[x] == x)
-		return x;
-	
-	return parent[x] = find(parent[x]);
+    if (p[x] < 0) return x;
+    return p[x] = find(p[x]);
 }
 
-void Union(int x, int y) {
-	x = find(x);
-	y = find(y);
-
-	if (x != y) {
-		if (x < y)
-			parent[y] = x;
-		else
-			parent[x] = y;
-	}
+bool uni(int u, int v) {
+    u = find(u);
+    v = find(v);
+    if (u == v) return false;    // 부모 같으면
+    p[u] = v;
+    return true;   // 부모 다르면 합치고 true 리턴 
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	
-	cin >> n >> m;
-	for (int i = 0; i <= n; i++)
-		parent[i] = i;
-	while (m--) {
-		int c, a, b;
-		cin >> c >> a >> b;
-		if (c == 0)
-			Union(a, b);
-		else {
-			if (find(a) == find(b))
-				cout << "YES\n";
-			else
-				cout << "NO\n";
-		}
-	}
+    ios_base::sync_with_stdio(0); cin.tie(0);
 
-	return 0;
+    cin >> n >> m;
+    // init
+    fill(p, p + n + 1, -1);
+    while (m--) {
+        cin >> cmd >> a >> b;
+        if (cmd == 0) { // union
+            uni(a, b);
+        } else {
+            if (find(a) == find(b)) { // 같은 그룹
+                cout << "yes\n";
+            } else {
+                cout << "no\n";
+            }
+        }
+    }
+
+    return 0;
 }
