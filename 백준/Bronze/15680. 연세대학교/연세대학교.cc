9cb9c2010be2613c899f07fc044e552e3db0961c
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-03-30 02:52:22 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+using namespace std;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    if (n == 0) cout << "YONSEI";
+    else cout << "Leading the Way to the Future";
+
+    return 0;
+}
