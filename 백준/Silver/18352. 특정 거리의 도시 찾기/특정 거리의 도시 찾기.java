74e3b515712f55f2eda96865c44bf15b60e9d6b8
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-05-07 22:34:34 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2944 ms, Memory: 381064 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,83 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.Collections;
+import java.util.Iterator;
+import java.awt.*;
+
+public class Main {
+    static class Pair {
+        int x;
+        int y;
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    static int n, m, k, x;
+    static int a, b;
+    static boolean[] visited;
+    static LinkedList<Integer> [] graph;
+    static LinkedList<Integer> ans = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
+        x = sc.nextInt();
+
+        visited = new boolean[n + 1];
+        graph = new LinkedList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new LinkedList<Integer>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            graph[a].add(b);
+        }
+
+        bfs(x);
+
+        if (ans.size() == 0) {
+            System.out.println("-1");
+        } else {
+            Collections.sort(ans);
+            Iterator<Integer> iter = ans.iterator();
+            while (iter.hasNext()) {
+                System.out.println(iter.next());
+            }
+        }
+    }
+
+    public static void bfs(int idx) {
+        Queue<Pair> q = new LinkedList<>();
+        visited[idx] = true;
+        q.offer(new Pair(idx, 0));
+
+        while (q.size() != 0) {
+            Pair cc = q.poll();
+            int cx = cc.x;
+            int cdis = cc.y;
+
+            Iterator<Integer> iter = graph[cx].listIterator();
+            while(iter.hasNext()) {
+                int nx = iter.next();
+                if (!visited[nx]) {
+                    visited[nx] = true;
+                    if (cdis == k-1) {
+                        ans.add(nx);
+                    } else {
+                        q.offer(new Pair(nx, cdis+1));
+                    }
+                }
+            }
+        }
+    }
+}
+
