0b31a8e896e6f0d88ef56ca661086243b693f8a3
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-06-05 14:30:09 +0900

    [Platinum V] Title: 배열에서 이동, Time: 4104 ms, Memory: 35044 KB -BaekjoonHub

---
+++ 백준/Platinum/1981. 배열에서 이동/배열에서 이동.py
@@ -0,0 +1,66 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+mn = arr[0][0]
+mx = arr[0][0]
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] > mx:
+            mx = arr[i][j]
+        if arr[i][j] < mn:
+            mn = arr[i][j]
+
+s = 0
+e = mx - mn
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(st , ed):
+    q = deque()
+    vis = [[0] * n for _ in range(n)]
+    if st <= arr[0][0] <= ed:
+        q.append((0, 0))
+        vis[0][0] = 1
+
+    while q:
+        x, y = q.pop()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n and vis[nx][ny] == 0:
+                if st <= arr[nx][ny] <= ed:
+                    if nx == n - 1 and ny == n - 1:
+                        return 1
+
+                    vis[nx][ny] = 1
+                    q.append((nx, ny))
+
+    return -1
+
+ans = -1
+
+# 이분 탐색으로 구간 범위 설정
+while s <= e:
+    mid = (s + e) // 2
+
+    # 시작점 설정
+    for j in range(mn, mx - mid + 1):
+        ret = bfs(j, j + mid)
+        if ret == 1:
+            ans = mid
+            break
+
+    if ans == mid:
+        e = mid - 1
+    else:
+        s = mid + 1
+
+print(ans)
