28fbf1a8e94ffa134345cd78a555527b5885ca64
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-07-22 11:50:50 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 73.24 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,3 +1,30 @@
+# two pointer
+
 def solution(gems):
-    answer = []
-    return answer
+    length = len(gems)
+    type_cnt = len(set(gems))
+    ans = [1, length]
+    ans_len = length
+    
+    st, ed = 0, 0
+    dic = {gems[0]: 1}
+    
+    while st <= ed and ed < length:
+        if len(dic) < type_cnt:
+            ed += 1
+            if ed == length:
+                break
+                
+            dic[gems[ed]] = dic.get(gems[ed], 0) + 1
+        else:
+            if ed - st + 1 < ans_len:
+                ans = [st + 1, ed + 1]
+                ans_len = ed - st + 1
+            
+            if dic[gems[st]] == 1:
+                dic.pop(gems[st])
+            else:
+                dic[gems[st]] -= 1            
+            st += 1
+            
+    return ans
