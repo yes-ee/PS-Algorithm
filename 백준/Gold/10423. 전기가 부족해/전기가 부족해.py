40f4707856925ccc3f9995a66ddf845333dcdcbc
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2025-08-30 19:45:31 +0900

    [Gold III] Title: 전기가 부족해, Time: 208 ms, Memory: 49604 KB -BaekjoonHub

--- 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
@@ -2,37 +2,89 @@
 # prim
 
 import sys
-import heapq as hq
+import heapq as h
+push = h.heappush
+pop = h.heappop
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 n, m, k = map(int, input().split())
 rt = list(map(int, input().split())) # 발전소 위치
-adj = [[] for i in range(n + 1)]
-vis = [0 for i in range(n + 1)]
-heap = []
-sum = 0
+pq = []
 
-for i in range(m):
-    a, b, c = map(int, input().split())
-    adj[a].append((c, b))
-    adj[b].append((c, a))
+def prim():
+    sum = 0
+    vis = [0 for i in range(n + 1)]
 
-# root에 연결된 애들 추가
-for cur in rt:
-    vis[cur] = 1
-    for nxt in adj[cur]:
-        hq.heappush(heap, nxt)
+    adj = [[] for i in range(n + 1)]
 
-while heap:
-    c, x = hq.heappop(heap)
+    for i in range(m):
+        a, b, c = map(int, input().split())
+        adj[a].append((c, b))
+        adj[b].append((c, a))
 
-    if vis[x]: continue
+    # root에 연결된 애들 추가
+    for cur in rt:
+        vis[cur] = 1
+        for nxt in adj[cur]:
+            push(pq, nxt)
 
-    vis[x] = 1
-    sum += c
+    while pq:
+        c, x = pop(pq)
 
-    for nc, nx in adj[x]:
-        if vis[nx]: continue
-        hq.heappush(heap, (nc, nx))
+        if vis[x]: continue
 
-print(sum)
+        vis[x] = 1
+        sum += c
+
+        for nc, nx in adj[x]:
+            if vis[nx]: continue
+            push(pq, (nc, nx))
+
+    print(sum)
+
+def kruskal():
+    sum = 0
+    p = [i for i in range(n+1)]
+    edges = []
+
+    for i in range(m):
+        a, b, c = map(int, input().split())
+        edges.append((c, a, b))
+
+    edges.sort()
+
+    # 발전소를 그룹 0번으로 묶음
+    for x in rt:
+        p[x] = 0
+
+    def find(u):
+        if p[u] == u:
+            return u
+        p[u] = find(p[u])
+        return p[u]
+
+    def uni(u, v):
+        u = find(u)
+        v = find(v)
+
+        if u == v:
+            return False
+        p[u] = v
+        return True
+
+    cnt = k
+
+    for edge in edges:
+        cost, x, y = edge
+
+        if uni(x, y):
+            cnt += 1
+            sum += cost
+
+            if cnt == n:
+                break
+
+    print(sum)
+
+kruskal()
